568116814f0ba939ee97ed610c416820013d84a8
vjourne <victor.journe@eleves.ec-nantes.fr>
2019-02-22 15:43:37 +0100

    env var

---
+++ environment.py
@@ -0,0 +1,11 @@
+import os
+
+ROOT_DIR = os.environ['ROOT_DIR']
+TMP_DIR = os.environ['TMP_DIR']
+DSS_DIR  = os.environ['DSS_DIR']
+
+API_KEY_VIT = os.environ['API_KEY_VIT']
+PROJECT_KEY_VIT = os.environ['PROJECT_KEY_VIT']
+
+DSS_HOST = os.environ['DSS_HOST']
+VERTICA_HOST = os.environ['VERTICA_HOST']
