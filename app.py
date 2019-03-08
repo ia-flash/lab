640e4297028947d653a679ca2aaa7e57d5eea09a
vjourne <victor.journe@elves.ec-nantes.fr>
2019-03-08 11:37:35 +0100

    app flask

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from PIL import Image
 from io import BytesIO
 from flask import Flask, render_template, Response, render_template_string, send_from_directory, request
 
+import pandas as pd
+from environment import ROOT_DIR
+
 WIDTH = 600
 HEIGHT = 800
 
@@ -56,8 +59,37 @@ def images():
         'images': images
     })
 
+@app.route('/csv/<path:csvpath>')
+def images_csv(csvpath):
+    print(csvpath)
+    images = []
+    limit=1e8
+    df = pd.read_csv(os.path.join('/',csvpath))
+    df = df[df['target']==0]
+    for i, row in df.iterrows():
+        if i > limit:
+            break
+        filename = os.path.join(ROOT_DIR,row['img_path'])
+        im = Image.open(filename)
+        w, h = im.size
+        aspect = 1.0*w/h
+        if aspect > 1.0*WIDTH/HEIGHT:
+            width = min(w, WIDTH)
+            height = width/aspect
+        else:
+            height = min(h, HEIGHT)
+            width = height*aspect
+        images.append({
+            'width': int(width),
+            'height': int(height),
+            'src': filename
+        })
+        i +=1
+        print(row['img_path'])
 
+    return render_template("preview.html", **{
+        'images': images
+    })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
-
+    app.run(host='0.0.0.0', threaded=True,debug=True)
