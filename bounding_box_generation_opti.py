668c023113407be43519877deb290c7068e13317
vjourne <victor.journe@elves.ec-nantes.fr>
2019-04-29 16:23:40 +0200

    write in database, ready to use

--- bounding_box_generation_opti.py
+++ bounding_box_generation_opti.py
@@ -7,7 +7,7 @@ from mmcv.image import imread, imwrite
 from mmdet.core import get_classes
 
 import mmcv
-from mmcv.runner import load_checkpoint, parallel_test
+from mmcv.runner import load_checkpoint#, parallel_test
 from mmcv.parallel import scatter, collate, MMDataParallel
 
 from mmdet.models import build_detector, detectors
@@ -31,6 +31,9 @@ class_to_keep = ['person','bicycle', 'car',
                 'motorcycle','bus',
                 'truck','traffic_light','stop_sign',
                 'parking_meter','bench']
+
+col_seg = ['img_name','path','x1','y1','x2','y2',
+                                    'class','score']
 
 def load_data(dataset_name = 'img_MIF',nrows=1e3):
     # cached the dataset as csv
@@ -64,13 +67,7 @@ def load_data(dataset_name = 'img_MIF',nrows=1e3):
 
     return img_df
 
-def send_data(results):
-    to_save = save_result(result,
-                class_to_keep=class_to_keep,
-                dataset='coco',
-                score_thr=0.3)
 
-    return 0
 
 def load_config(modele):
 
@@ -82,36 +79,107 @@ def load_config(modele):
 
 def single_test(model, data_loader, show=False):
     model.eval()
-    results = []
+    results, logs= [], []
     dataset = data_loader.dataset
     prog_bar = mmcv.ProgressBar(len(dataset))
-    for row, data in enumerate(dataset, data_loader):
-        with torch.no_grad():
-            result = model(return_loss=False, rescale=not show, **data)
-        results.append(result)
+    for i, data in enumerate(data_loader):
+        traceback=''
+        try :
+            with torch.no_grad():
+                result = model(return_loss=False, rescale=not show, **data)
 
-        if show:
-            model.module.show_result(data, result, dataset.img_norm_cfg,
-                                     dataset=dataset.CLASSES)
+
+            if show:
+                model.module.show_result(data, result, dataset.img_norm_cfg,
+                                         dataset=dataset.CLASSES)
+
+        except Exception as traceback:
+            print(traceback)
+            result = []
+
+        logs.append(traceback)
+        results.append(result)
 
         batch_size = data['img'][0].size(0)
         for _ in range(batch_size):
             prog_bar.update()
 
-        #print(data['img_meta'][0].data )
-        # print row
-        
-        if i %100 == 0:
-            print('Send 100 rows')
-            to_save = save_result(result,
-                        class_to_keep=class_to_keep,
-                        dataset='coco',
-                        score_thr=0.3)
-            #send_data(results)
-            results = []
 
-    return results
+    return results, logs
 
+import multiprocessing
+def worker_func(model_cls, model_kwargs, checkpoint, dataset, data_func,
+                gpu_id, idx_queue, result_queue):
+    model = model_cls(**model_kwargs)
+    load_checkpoint(model, checkpoint, map_location='cpu')
+    torch.cuda.set_device(gpu_id)
+    model.cuda()
+    model.eval()
+    with torch.no_grad():
+        while True:
+            idx = idx_queue.get()
+            data = dataset[idx]
+            log = ''
+            try:
+                result = model(**data_func(data, gpu_id))
+            except Exception as log:
+                result = []
+
+            result_queue.put((idx, result, log))
+
+
+def parallel_test(model_cls,
+                  model_kwargs,
+                  checkpoint,
+                  dataset,
+                  data_func,
+                  gpus,
+                  workers_per_gpu=1):
+    """Parallel testing on multiple GPUs.
+    Args:
+        model_cls (type): Model class type.
+        model_kwargs (dict): Arguments to init the model.
+        checkpoint (str): Checkpoint filepath.
+        dataset (:obj:`Dataset`): The dataset to be tested.
+        data_func (callable): The function that generates model inputs.
+        gpus (list[int]): GPU ids to be used.
+        workers_per_gpu (int): Number of processes on each GPU. It is possible
+            to run multiple workers on each GPU.
+    Returns:
+        list: Test results.
+    """
+    ctx = multiprocessing.get_context('spawn')
+    idx_queue = ctx.Queue()
+    result_queue = ctx.Queue()
+    num_workers = len(gpus) * workers_per_gpu
+    workers = [
+        ctx.Process(
+            target=worker_func,
+            args=(model_cls, model_kwargs, checkpoint, dataset, data_func,
+                  gpus[i % len(gpus)], idx_queue, result_queue))
+        for i in range(num_workers)
+    ]
+    for w in workers:
+        w.daemon = True
+        w.start()
+
+    for i in range(len(dataset)):
+        idx_queue.put(i)
+
+    results = [None for _ in range(len(dataset))]
+    logs = [None for _ in range(len(dataset))]
+
+    prog_bar = mmcv.ProgressBar(task_num=len(dataset))
+    for _ in range(len(dataset)):
+        idx, res, log = result_queue.get()
+        results[idx] = res
+        logs[idx] = log
+        prog_bar.update()
+    print('\n')
+    for worker in workers:
+        worker.terminate()
+
+    return results, logs
 
 
 def det_bboxes(bboxes,
@@ -208,22 +276,7 @@ def test_CustomDataset():
     first_element = car_dataset[0]
     print(first_element)
 
-def main():
-    # Build dataset
-
-    img_df = load_data(nrows=500)
-    cfg = load_config(modele)
-    car_dataset = CustomDataset(img_df, ROOT_DIR,**cfg.data.val)
-    print('length of dataset :')
-    print(len(car_dataset))
-
-
-    # build modele
-
-    gpus = 1
-    workers_per_gpu = 1
-
-    checkpoint = '/model/%s.pth'%modele['checkpoint']
+def run_detection(cfg,car_dataset, gpus ,workers_per_gpu, checkpoint):
 
     if gpus == 1:
         show = False
@@ -254,12 +307,62 @@ def main():
             range(gpus),
             workers_per_gpu=workers_per_gpu)
 
+    return outputs
 
-    for row, results  in zip(img_df.iterrows(),outputs):
-        to_save = save_result(results,
-                    class_to_keep=class_to_keep,
-                    dataset='coco',
-                    score_thr=0.3)
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+def main():
+    # Build dataset
+    gpus = 4
+    workers_per_gpu = 2
+    checkpoint = '/model/%s.pth'%modele['checkpoint']
+
+    cfg = load_config(modele)
+
+    img_df = load_data(nrows=100)
+
+
+    col_seg = ['img_name','path','x1','y1','x2','y2',
+                                    'class','score']
+
+
+
+    for chunk_df in chunker(img_df, 10):
+        car_dataset = CustomDataset(chunk_df, ROOT_DIR,**cfg.data.val)
+        print('length of dataset :')
+        print(len(car_dataset))
+
+
+        outputs, logs = run_detection(cfg, car_dataset, gpus ,workers_per_gpu, checkpoint)
+
+        img_seg = pd.DataFrame(columns=col_seg)
+        log_seg = pd.DataFrame(columns=['img_name','path','traceback'])
+
+        for (_,row), results, traceback  in zip(chunk_df.iterrows(), outputs, logs):
+            to_save = save_result(results,
+                        class_to_keep=class_to_keep,
+                        dataset='coco',
+                        score_thr=0.3)
+            to_save_df = pd.DataFrame(to_save)
+            to_save_df['img_name'] = row['img_name']
+            to_save_df['path'] = row['path']
+
+            img_seg = pd.concat([img_seg, to_save_df],
+                    ignore_index=True,sort=False)
+
+
+
+            log_seg = log_seg.append(dict(img_name=row['img_name'],
+                                 path=row['path'],
+                                 traceback=traceback),
+                         ignore_index=True)
+
+        img_seg['score'] = img_seg['score'].round(decimals=2)
+        print('%s images processed'%log_seg.shape[0])
+        write_dataframe(VERTICA_HOST,PROJECT_KEY_VIT,'log_%s'%modele['conf'],log_seg)
+        write_dataframe(VERTICA_HOST,PROJECT_KEY_VIT,'box_%s'%modele['conf'],img_seg[col_seg])
+
 
 
 
